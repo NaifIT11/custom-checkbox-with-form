3e1fd03dff90156cd524657d75700ab8b95a9da3
Naif Otibi <nn.n@outlook.cl>
2024-10-14 22:44:46 +0300

    adding the submit button

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -9,11 +9,12 @@ export default function Form() {
         e.preventDefault();
     }
   return <div className="center p-4 rounded space-y-4 border">
-    <form className="flex gap-4" onSubmit={handleSubmit}>
+    <form className="flex gap-4 flex-col" onSubmit={handleSubmit}>
         <div className="flex gap-2 items-center">
             <Checkbox checked={checked} onCheckedChange={setChecked} />
             <p className="text-sm">I accept organization terms and conditions</p>
         </div>
+        <button type="submit" className="px-4 py-2 rounded bg-green-300 text-white">submit</button>
     </form>
   </div>;
 }
