6421e502f6da196b1065ddd997b416857b99755a
Naif Otibi <nn.n@outlook.cl>
2024-10-14 22:28:05 +0300

    adding state

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { useState } from "react";
 import Checkbox from "./Checkbox";
 
 export default function Form() {
-  return <div className="center p-4 rounded">
-    <form>
-        
+    const [checked , setChecked] = useState<boolean>(false);
+  return <div className="center p-4 rounded space-y-4">
+    <form className="flex gap-4">
+        <div className="flex gap-2">
+            <Checkbox checked={checked} onCheckedChange={setChecked} />
+            <p className="text-sm">I accept organization terms and conditions</p>
+        </div>
     </form>
   </div>;
 }
